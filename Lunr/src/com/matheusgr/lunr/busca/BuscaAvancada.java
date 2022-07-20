b7ccfbdf2222a917831cdcd1bf2feed86d60c0fc
João Henrique Almeida Xavier <Jhenriquealmeidaxavier@hotmail.com>
2022-07-20 10:07:33 -0300

    reset busca avancada

--- Lunr/src/com/matheusgr/lunr/busca/BuscaAvancada.java
+++ Lunr/src/com/matheusgr/lunr/busca/BuscaAvancada.java
@@ -1,19 +1,14 @@
 package com.matheusgr.lunr.busca;
 
-import java.util.HashMap;
-import java.util.HashSet;
+
 import java.util.Map;
-import java.util.Set;
+
 
 import com.matheusgr.lunr.documento.Documento;
 
 public class BuscaAvancada {
 
 	private Map<String, String> termos;
-
-	// Não estou sabendo cadastrar os meus termos ai dentro, já que
-	// a estrututa precisa ser do tipo chave valor, pensei em usar String[0] e
-	// String[1]
 
 	public BuscaAvancada(Map<String, String> metaDados) {
 		(new ValidadorBusca()).valida(termos);
@@ -23,25 +18,7 @@ public class BuscaAvancada {
 
 	public Map<Documento, Integer> Busca(Map<String, String> meta) {
 
-		// por que não posso instanciar com Documento
 
-		HashMap<String, Documento> documentos = new HashMap<String, Documento>();
-
-		Set<Documento> listaMatch = new HashSet<>();
-
-		Set<String> chaves = documentos.keySet();
-
-		for (String chave : chaves) {
-			if (chave != null) {
-
-				for (int i = 0; i < documentos.size(); i++) {
-					String[] texto = documentos.get(chave).getTexto();
-					
-					//aqui precisa comparar o texto com o hash map passado MET
-				}
-
-			}
-		}
 
 		return null;
 	}
